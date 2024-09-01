5abc6922abad692ffaf978611f7eb230ff215046
Gabriel Rosa <81601748+gfreitasrosa@users.noreply.github.com>
2024-08-31 23:08:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
 
     try {
         // Faz a requisição ao backend
-        const response = await fetch('http://localhost:8000/api/login/', {
+        const response = await fetch('https://525f-2804-7f0-93c0-4ba0-f11a-3ffe-9590-3cbf.ngrok-free.app', {
+            // http://localhost:8000/api/login/
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
